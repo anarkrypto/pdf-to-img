45fd8b15b75df09711ae5448db3dabf94831b7e1
anarkrypto <anarkrypto@gmail.com>
2023-12-18 01:59:07 -0300

    add utils

---
+++ src/utils/upload.ts
@@ -0,0 +1,36 @@
+import { S3 } from 'aws-sdk'
+import { createReadStream } from 'fs'
+
+const accessKeyId = process.env.S3_ACCESS_KEY_ID!
+const secretAccessKey = process.env.S3_SECRET_ACCESS_KEY!
+const bucketName = process.env.S3_BUCKET_NAME!
+
+const s3 = new S3({
+  accessKeyId,
+  secretAccessKey,
+})
+
+export async function uploadFile(path: string, key: string, type?: string): Promise<S3.ManagedUpload.SendData> {
+  return new Promise((resolve, reject) => {
+    const fileStream = createReadStream(path)
+
+    fileStream.on('error', reject)
+
+    s3.upload(
+      {
+        Bucket: bucketName,
+        Key: key,
+        Body: fileStream,
+        ACL: 'public-read',
+        ContentType: type
+      },
+      undefined,
+      (error, data) => {
+        if (error) {
+          return reject(error.message)
+        }
+        resolve(data)
+      },
+    )
+  })
+}
