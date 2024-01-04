d31f18dfd4b91ef2035d22e62084b35dded22c66
anarkrypto <anarkrypto@gmail.com>
2024-01-04 18:32:02 -0300

    add common types

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+export type ImageFormats = 'png' | 'jpg' | 'jpeg' | 'webp'
+
+export interface PageResult {
+    page: number
+    url: string
+}
+
+export interface ConversionOptions {
+    quality: number
+    format: ImageFormats
+    dpi: number
+}
+
+export interface TaskPayload extends ConversionOptions {
+    convertionId: string
+    url: string
+    pages: PageResult[]
+}
