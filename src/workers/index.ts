55fec3f9f3b973386f01b603991cca8a5d417316
anarkrypto <anarkrypto@gmail.com>
2024-01-05 17:47:50 -0300

    move outputDir delete command

--- src/workers/index.ts
+++ src/workers/index.ts
@@ -3,6 +3,10 @@ import { sendToQueue } from '../queue/produce'
 import { TaskPayload } from '../types'
 import { convert } from './convert'
 import { uploadImages } from './upload'
+import { promisify } from 'util'
+import { exec } from 'child_process'
+
+const execAsync = promisify(exec)
 
 async function convertTask(payload: TaskPayload) {
   const pages = await convert(payload)
@@ -16,6 +20,7 @@ async function convertTask(payload: TaskPayload) {
 async function uploadTask(payload: TaskPayload) {
   await uploadImages(payload)
   console.log(`[INFO] Upload task ${payload.convertionId} finished`)
+  await execAsync(`rm -rf /tmp/${payload.convertionId}`)
 }
 
 export async function startWorkers() {
--- src/workers/upload.ts
+++ src/workers/upload.ts
@@ -1,16 +1,12 @@
 import { S3 } from 'aws-sdk'
 import { createReadStream } from 'fs'
 import { PageResult, TaskPayload } from '../types'
-import { promisify } from 'util'
-import { exec } from 'child_process'
 
 const BUCKET_DIR = 'pdfs'
 
 const accessKeyId = process.env.S3_ACCESS_KEY_ID!
 const secretAccessKey = process.env.S3_SECRET_ACCESS_KEY!
 const bucketName = process.env.S3_BUCKET_NAME!
-
-const execAsync = promisify(exec)
 
 const s3 = new S3({
   accessKeyId,
@@ -51,25 +47,20 @@ export async function uploadImages({
   pages,
   format,
 }: TaskPayload) {
-  try {
-    const promises: Promise<PageResult>[] = []
+  const promises: Promise<PageResult>[] = []
 
-    for (const { page, url } of pages) {
-      const key = `${BUCKET_DIR}/${convertionId}/${page}.${format}`
-      const promise = uploadFile(
-        url,
-        key,
-        `image/${format}`,
-      ).then((url) => ({ url, page: page }))
-      promises.push(promise)
-    }
-
-    const result = await Promise.all(promises)
-
-    const pagesSorted = result.sort((a, b) => a.page - b.page)
-
-    return pagesSorted
-  } finally {
-    await execAsync(`rm -rf /tmp/${convertionId}`)
+  for (const { page, url } of pages) {
+    const key = `${BUCKET_DIR}/${convertionId}/${page}.${format}`
+    const promise = uploadFile(url, key, `image/${format}`).then((url) => ({
+      url,
+      page: page,
+    }))
+    promises.push(promise)
   }
+
+  const result = await Promise.all(promises)
+
+  const pagesSorted = result.sort((a, b) => a.page - b.page)
+
+  return pagesSorted
 }
