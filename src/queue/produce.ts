fb40335a3ad7558c9d8e44965730995666d76ce2
anarkrypto <anarkrypto@gmail.com>
2024-01-10 20:11:50 -0300

    update producer: assert exchanges

--- src/queue/produce.ts
+++ src/queue/produce.ts
@@ -1,15 +1,21 @@
 import { Connection, connect } from './connect'
 import { TaskPayload } from '../types'
 
-
 export async function sendToQueue(queue: string, task: TaskPayload) {
   const connection: Connection = await connect()
 
-
   const channel = await connection.createChannel()
-  
+
   try {
-    await channel.assertQueue(queue, { durable: true })
+    await Promise.all([
+      channel.assertExchange('notification-exchange', 'fanout'),
+      channel.assertExchange('error-exchange', 'fanout'),
+    ])
+
+    await channel.assertQueue(queue, {
+      durable: true,
+      deadLetterExchange: 'error-exchange',
+    })
     await channel.sendToQueue(queue, Buffer.from(JSON.stringify(task)))
   } finally {
     await channel.close()
